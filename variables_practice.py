d3eb3e7181e8c7f0df515948e4ffaf4f537f9625
Hazelnut713 <gabewalton@ucas-edu.net>
2026-09-10 13:44:31 -0600

    variables

--- variables_practice.py
+++ variables_practice.py
@@ -1,8 +1,8 @@
 # GW, Variables Practice
 
 name = input("What is your Name? ")
-number_one = input("Put a number between 1-10 ")
-number_two = input("Put a nuber between 100-1000 ")
+number_one = input("Put a number between 1-10: ")
+number_two = input("Put a nuber between 100-1000: ")
 breakfast = input("What did you eat for breakfast? ")
 color = input("What is your favorite color? ")
 school = input("What school do you go to? ")
