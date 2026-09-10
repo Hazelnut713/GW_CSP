eaecedaf46f926ce44ec2443ed67bbd323691e2b
Hazelnut713 <gabewalton@ucas-edu.net>
2026-09-10 13:21:24 -0600

    variables

--- variables_practice.py
+++ variables_practice.py
@@ -1,4 +1,14 @@
 # GW, Variables Practice
 
-name = Sheldon
-number_one = 8
+name = "Sheldon,"
+number_one = "8,"
+number_two = "42,"
+breakfast = "eggs and hashbrowns,"
+color = "purple,"
+school = "Lakeridge JR. High,"
+year = "2026,"
+eye = "green,"
+age = "15,"
+subject = "Math."
+
+print(name, number_one, number_two, breakfast, color, school, year, eye, age,subject)
