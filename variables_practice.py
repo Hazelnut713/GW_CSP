a964d0077daf975cbaf6b37b7cc7f7a66fe2341a
Hazelnut713 <gabewalton@ucas-edu.net>
2026-09-10 13:40:25 -0600

    variables

--- variables_practice.py
+++ variables_practice.py
@@ -1,14 +1,14 @@
 # GW, Variables Practice
 
-name = "Sheldon,"
-number_one = "8,"
-number_two = "42,"
-breakfast = "eggs and hashbrowns,"
-color = "purple,"
-school = "Lakeridge JR. High,"
-year = "2026,"
-eye = "green,"
-age = "15,"
-subject = "Math."
+name = input("What is your Name? ")
+number_one = input("Put a number between 1-10 ")
+number_two = input("Put a nuber between 100-1000 ")
+breakfast = input("What did you eat for breakfast? ")
+color = input("What is your favorite color? ")
+school = input("What school do you go to? ")
+year = input("What year is it? ")
+eye = input("What is your eye color? ")
+age = input("How old are you? ")
+subject = input("What is your favorite subject? ")
 
 print(name, number_one, number_two, breakfast, color, school, year, eye, age,subject)
