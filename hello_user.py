aee542a75bd817a80ffa822a4861cdb8c9be4cd0
Hazelnut713 <gabe.walton@ucas-edu.net>
2026-09-16 18:09:21 +0000

    Hello User

--- hello_user.py
+++ hello_user.py
@@ -1,13 +1,11 @@
 # GW Hello User
 
 while True:
-    first_name = input("What is your first name: ").title().strip()
-    last_name = input("What is your last name").title().strip()
-    if first_name.isnumeric() + last_name.isnumeric():
+    name = input("What is your name: ").title().strip()
+    if name.isnumeric()
         print("Sorry that is not a name")
-    elif " " in first_name:
         break
     else:
-        print("I said your first and last name")
+        print("I want to know your name not a number.")
 
 print(f"hello {name}!")
