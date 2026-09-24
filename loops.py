9977648ee8e3884b1f35fbbcfab8bf7a9ee24019
Hazelnut713 <gabewalton@ucas-edu.net>
2026-09-24 13:51:36 -0600

    Loops

---
+++ loops.py
@@ -0,0 +1,22 @@
+# GW, Loop Notes
+import random
+#code that will repeat over and over again
+
+count = 1
+
+while count <= 10:
+    print(count)
+    count += 1
+
+goose = random.randint(1,11)
+ducks = 1
+
+while True:
+    print("duck")
+    if ducks == goose:
+        break
+    ducks += 1
+print("GOOSE!!!!")
+
+
+siblings = ["Alex", "Kaite", "Andrew", "Tia", "Treyson", "Xaiver", "Jake"]
